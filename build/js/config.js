32273391748af7fd7cc6da3aab855a7857ba3008
Mark Lundin <mark@mark-lundin.com>
2013-08-05 19:18:59 +0100

    more pathing issues trying to be solved

--- build/js/config.js
+++ build/js/config.js
@@ -13,27 +13,27 @@ requirejs.config( {
 
 	shim:{
 
-		"./libs/threejs/build/three.js":{
+		"../libs/threejs/build/three.js":{
 			exports: "THREE"
 		},
 
-		"./libs/threejs/examples/js/postprocessing/EffectComposer": [
-			"./libs/threejs/examples/js/shaders/CopyShader", 
-			"./libs/threejs/examples/js/postprocessing/ShaderPass",
-			"./libs/threejs/examples/js/postprocessing/RenderPass",
-			"./libs/threejs/examples/js/shaders/SSAOShader",
-			"./libs/threejs/examples/js/postprocessing/MaskPass"
+		"../libs/threejs/examples/js/postprocessing/EffectComposer": [
+			"../libs/threejs/examples/js/shaders/CopyShader", 
+			"..//libs/threejs/examples/js/postprocessing/ShaderPass",
+			"../libs/threejs/examples/js/postprocessing/RenderPass",
+			"../libs/threejs/examples/js/shaders/SSAOShader",
+			"../libs/threejs/examples/js/postprocessing/MaskPass"
 		],
 
-		"./libs/threejs/examples/js/controls/OrbitControls.js": ['./libs/threejs/build/three.js'] ,
-		"./libs/threejs/examples/js/shaders/SSAOShader.js": ['./libs/threejs/build/three.js'] ,
-		"./libs/threejs/examples/js/shaders/CopyShader.js": ['./libs/threejs/build/three.js'] ,
-		"./libs/threejs/examples/js/postprocessing/RenderPass.js": ['./libs/threejs/build/three.js'] ,
-		"./libs/threejs/examples/js/postprocessing/ShaderPass.js": ['./libs/threejs/build/three.js'],
-		"./libs/threejs/examples/js/postprocessing/MaskPass.js":['./libs/threejs/build/three.js'],
-		"./libs/threejs/examples/js/controls/TransformControls.js":['./libs/threejs/build/three.js'],
+		"../libs/threejs/examples/js/controls/OrbitControls.js": ['../libs/threejs/build/three.js'] ,
+		"../libs/threejs/examples/js/shaders/SSAOShader.js": ['../libs/threejs/build/three.js'] ,
+		"../libs/threejs/examples/js/shaders/CopyShader.js": ['../libs/threejs/build/three.js'] ,
+		"../libs/threejs/examples/js/postprocessing/RenderPass.js": ['../libs/threejs/build/three.js'] ,
+		"../libs/threejs/examples/js/postprocessing/ShaderPass.js": ['../libs/threejs/build/three.js'],
+		"../libs/threejs/examples/js/postprocessing/MaskPass.js":['../libs/threejs/build/three.js'],
+		"../libs/threejs/examples/js/controls/TransformControls.js":['../libs/threejs/build/three.js'],
 		
-		"./libs/threejs/examples/js/ImprovedNoise": {
+		"../libs/threejs/examples/js/ImprovedNoise": {
 			exports:"ImprovedNoise"
 		}
 	
