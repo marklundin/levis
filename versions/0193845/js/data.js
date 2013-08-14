15056c44708fab348f6f56a5cf7403a81c32dd94
Mark Lundin <mark@mark-lundin.com>
2013-08-14 12:34:45 +0100

    snaphot

---
+++ versions/0193845/js/data.js
@@ -0,0 +1,45 @@
+define(['module'], function (module) {
+
+	
+	var api =  function( callback, errCallback ) {
+
+	    require([ 
+	    	module.config().instagram.feed + "&callback=define",
+	    	module.config().twitter.feed + "&callback=define",
+	    	], function( instagramData, twitterData ){
+
+	    		twitterData.loaded 		= twitterData.status === 'OK';
+	    		instagramData.loaded 	= instagramData.status === 'OK';
+	    		var loaded = instagramData.loaded && twitterData.loaded;
+
+	    		if( loaded ) callback( twitterData, instagramData )
+	    		else errCallback( instagramData, twitterData );
+
+	    });
+
+	}
+
+	api.search = function( term, callback ){
+
+
+		require([ 
+	    	module.config().instagram.search + "&callback=define",
+	    	module.config().twitter.search + "&callback=define",
+	    	], function( instagramData, twitterData ){
+
+	    		twitterData.loaded 		= twitterData.status === 'OK';
+	    		instagramData.loaded 	= instagramData.status === 'OK';
+	    		var loaded = instagramData.loaded && twitterData.loaded;
+
+	    		if( loaded ) callback( twitterData, instagramData )
+	    		else errCallback( instagramData, twitterData );
+
+	    });
+
+
+	}
+
+	return api;
+
+
+})
